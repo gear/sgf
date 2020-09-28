f34354295a32ce9b99e843f61366a5071ff237eb
gear <me@gearons.org>
2020-09-28 12:33:48 +0900

    add horizontal stacking

--- model.py
+++ model.py
@@ -33,13 +33,12 @@ class SGF(nn.Module):
         return F.log_softmax(y_hat, dim=1)
 
 
-# SGF-B in paper
-class SGFB(nn.Module):
+class SGFS(nn.Module):
     def __init__(self, nfeat, nlayers, nhidden, nclass, dropout):
-        super(SGFB, self).__init__()
+        super(SGFS, self).__init__()
         self.filters = nn.ModuleList()
         for _ in range(nlayers):
-            self.filters.append(GraphFilter(0.5, 0.5, "B"))
+            self.filters.append(GraphFilterS(0.))
         self.fc_in = nn.Linear(nfeat, nhidden)
         self.fc_out = nn.Linear(nhidden, nclass)
         self.params1 = list(self.filters.parameters())
@@ -56,10 +55,9 @@ class SGFB(nn.Module):
         skip_accum = H_0
         for i, filt in enumerate(self.filters):
             H_l = F.dropout(H_l, self.dropout, training=self.training)
-            H_l, skip = filt(H_l, adj, H_0)
-            if i < self.nlayers-1:
-                skip_accum += skip
-        H_l = F.dropout(H_l+skip_accum, self.dropout, training=self.training)
+            H_l, skip = filt(H_l, adj)
+            skip_accum += skip
+        H_l = F.dropout(skip_accum, self.dropout, training=self.training)
         y_hat = self.fc_out(H_l)
         return F.log_softmax(y_hat, dim=1)
 
@@ -86,3 +84,13 @@ class GraphFilter(nn.Module):
             return self._skip_from_input(inp, adj, x)
         else:
             return self._skip_to_output(inp, adj, x)
+
+
+class GraphFilterS(nn.Module):
+    def __init__(self, alpha, *args, **kwargs):
+        super(GraphFilterS, self).__init__()
+        self.alpha = Parameter(torch.FloatTensor([alpha]))
+
+    def forward(self, inp, M):
+        H_l = torch.spmm(M, inp)
+        return H_l, self.alpha * H_l
